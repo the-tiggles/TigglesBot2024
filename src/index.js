2d2bb54445486ab23a63b9b54ae08a7c947f1374
Mark <palokid@gmail.com>
2024-04-02 00:12:54 -0400

    💻🐧 Update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-const { Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions } = require('discord.js');
+const { Client, Events, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
-client.on('ready', () => {
+client.once(Events.ClientReady, readyClient => {
     console.log('Bot is ready!');
+    console.log(`${readyClient.user.tag}`);
     client.user.setActivity('Just Tiggin arround');
 });
 
