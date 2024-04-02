a842486cbf7aaf672dd28de1609794f506b78af1
Mark <palokid@gmail.com>
2024-04-02 18:56:20 -0400

    💻🐧 Update

--- src/action.js
+++
@@ -1,9 +0,0 @@
-const core  = require('@actions/core');
-const github = require('@actions/github');
-async function run() {
-  console.log('hello world');
-  const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
-  // console.log(GITHUB_TOKEN);
-}
-
-run();
