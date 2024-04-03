6bb5230a61ac4a1c7b3b7c809f9f331831ad876e
Mark <palokid@gmail.com>
2024-04-02 23:45:48 -0400

    💻🐧 Update

--- src/commands/tools/cute.js
+++ src/commands/tools/cute.js
@@ -1,30 +1,26 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const superagent = require('superagent');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('cute')
     .setDescription('show me the cuteness!'),
-    async execute(interaction, client) {
-      const message = await interaction.deferReply({
-        fetchReply: true,
-      });
-      const { body } = await superagent.get('https://www.reddit.com/r/aww.json');
-      function getImage() {
-
-        var randIMG = body.data.children[Math.floor(Math.random() * body.data.children.length)].data.url;
-
-        if ((randIMG.includes('i.redd')) || (randIMG.includes('i.imgur'))) {
-            console.log('found this ' + randIMG);
-            message.channel.send('cute :heart_eyes: ' + randIMG);
+    async execute(interaction) {
+        getCuteImage();
+        function getCuteImage() {
+            let resultObject;
+            do {
+                const result = await redditParse.randomPost('cats');
+                resultObject = JSON.parse(result);
+            } while (resultObject.type != "image")
+            console.log(resultObject);
+            const kittenEmbed = new EmbedBuilder()
+                .setTitle(resultObject.title)
+                .setImage(resultObject.image)
+                .setColor(0x93c54b)
+            interaction.reply({
+                embeds: [kittenEmbed],
+            })
         }
-        else {
-            console.log('this is not what i want - let us try again');
-            return getImage(body);
-        }
-      }
-      await interaction.editReply({
-        content: getImage(body),
-      });
     },
 };
