b7ba94d0c8a1dc7b29fc2985185edf5db67b7b11
Mark <palokid@gmail.com>
2024-04-02 23:48:26 -0400

    💻🐧 Update

--- src/commands/tools/cute.js
+++ src/commands/tools/cute.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const superagent = require('superagent');
+const redditParse = require('redditparse');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -7,12 +7,12 @@ module.exports = {
     .setDescription('show me the cuteness!'),
     async execute(interaction) {
         getCuteImage();
+        const result = await redditParse.randomPost('cats');
         function getCuteImage() {
             let resultObject;
             do {
-                const result = await redditParse.randomPost('cats');
                 resultObject = JSON.parse(result);
-            } while (resultObject.type != "image")
+            } while (resultObject.type != 'image')
             console.log(resultObject);
             const kittenEmbed = new EmbedBuilder()
                 .setTitle(resultObject.title)
@@ -20,7 +20,7 @@ module.exports = {
                 .setColor(0x93c54b)
             interaction.reply({
                 embeds: [kittenEmbed],
-            })
+            });
         }
     },
 };
