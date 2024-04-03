70d7258ba12cb7027a52ead580eaeefb79698bec
Mark <palokid@gmail.com>
2024-04-02 23:57:11 -0400

    💻🐧 Update

--- src/commands/tools/cute.js
+++ src/commands/tools/cute.js
@@ -6,21 +6,21 @@ module.exports = {
     .setName('cute')
     .setDescription('show me the cuteness!'),
     async execute(interaction) {
-        getCuteImage();
         const result = await redditParse.randomPost('cats');
-        function getCuteImage() {
-            let resultObject;
-            do {
-                resultObject = JSON.parse(result);
-            } while (resultObject.type != 'image')
-            console.log(resultObject);
-            const kittenEmbed = new EmbedBuilder()
-                .setTitle(resultObject.title)
-                .setImage(resultObject.image)
-                .setColor(0x93c54b)
-            interaction.reply({
-                embeds: [kittenEmbed],
-            });
-        }
+        let resultObject;
+        do {
+            resultObject = JSON.parse(result);
+        } while (resultObject.type != 'image')
+        console.log(resultObject);
+        const kittenEmbed = new EmbedBuilder()
+            .setTitle(resultObject.title)
+            .setImage(resultObject.image)
+            .setColor(0x93c54b);
+
+        await interaction.reply({
+            embeds: [kittenEmbed],
+            ephemeral: true,
+
+        });
     },
 };
